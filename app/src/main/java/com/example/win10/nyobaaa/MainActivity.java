ed8925b2f39bf18b888747b18fee52cc115ac073
FarouqAlfi <45782257+FarouqAlfi@users.noreply.github.com>
2018-12-26 14:22:46 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/win10/nyobaaa/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.win10.nyobaaa;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    View view;
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        view    =   this.getWindow().getDecorView();
+        view.setBackgroundResource(R.color.hitam);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new dasbor_fragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_dasbor);
+        }
+    }
+
+    public void hijau(View view){
+        view.setBackgroundResource(R.color.colorPrimary);
+    }
+    public void biru_l(View view){
+        view.setBackgroundResource(R.color.blue_light);
+    }
+    public void birutua(View view){
+        view.setBackgroundResource(R.color.bagtoolbar);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_dasbor:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new dasbor_fragment()).commit();
+                break;
+            case R.id.nav_pertanian:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new pertanian_fragment()).commit();
+                break;
+            case R.id.nav_perkebunan:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new perkebunan_fragment()).commit();
+                break;
+            case R.id.nav_perhutanan:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new perhutanan_fragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/win10/nyobaaa/perhutanan_fragment.java
@@ -0,0 +1,17 @@
+package com.example.win10.nyobaaa;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class perhutanan_fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_perhutanan,container,false);
+    }
+}
