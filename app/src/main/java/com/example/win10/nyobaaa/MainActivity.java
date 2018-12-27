8632d48a6ae84e1c8c18df8e71745a921b5ebaa8
FarouqAlfi <45782257+FarouqAlfi@users.noreply.github.com>
2018-12-27 17:09:44 +0700

    Penambahan comment dan revisi menjadi lebih baik

--- app/src/main/java/com/example/win10/nyobaaa/MainActivity.java
+++ app/src/main/java/com/example/win10/nyobaaa/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,36 +21,37 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Super code goes here..
-            for (int a = 0; a < 10; a++)
-                // do something cool here
-/*<<<<<<< Updated upstream*/
-            {
-                //Someone else fix my bugs
-            }
-/*>>>>>>> Stashed changes*/
-
         view    =   this.getWindow().getDecorView();
         view.setBackgroundResource(R.color.hitam);
 
+        // Menginisasi Drawer Layout dan ActionBarToggle
         drawer = findViewById(R.id.drawer_layout);
+
+        // Menginisiasi  NavigationView
         NavigationView navigationView = findViewById(R.id.nav_view);
+        //Mengatur Navigasi View Item yang akan dipanggil untuk menangani item klik menu navigasi
         navigationView.setNavigationItemSelectedListener(this);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new dasbor_fragment()).commit();
+
+            // Membuat halaman pertama yang dibuka adalah nav_dasbor
             navigationView.setCheckedItem(R.id.nav_dasbor);
         }
     }
 
+    // Mengatur Button Warna setelah di pencet (On Click)
     public void birutua(View view){
         view.setBackgroundResource(R.color.bagtoolbar);
     }
-
+    // Metod ini akan memicu item Klik menu navigasi
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        //Memeriksa untuk melihat item yang akan dilklik dan melalukan aksi
         switch (item.getItemId()) {
+            // pilihan menu item navigasi akan menampilkan sesuai halaman pilihan ketika di klik
             case R.id.nav_dasbor:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new dasbor_fragment()).commit();
@@ -67,13 +69,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                         new perhutanan_fragment()).commit();
                 break;
         }
-
+        // Kode di sini akan merespons setelah drawer menutup disini kita biarkan kosong
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
     @Override
     public void onBackPressed() {
+        //  Kode di sini akan merespons setelah drawer terbuka disini kita biarkan kosong
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
